e6b26af432ee3d00eef9d08b12dee937c470e75b
ses414 <woodruffs1010@students.bowiestate.edu>
2018-01-30 13:21:05 -0500

    update googleAPI

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,5 +5,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '1066924482172-0n1h2rvtckk4dtouooll7talqgc41ab9.apps.googleusercontent.com', 'gRFmg5GAsT_Ll4NyT8lM0-yW', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_SECRET'], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
